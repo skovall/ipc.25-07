1f7bac27b1ab6d352160f78b931d43eb7b3eb3e8
skovall <stetskovall@gmail.com>
2026-04-01 13:31:47 +0300

    Update ip_geolocation_export.h

--- IP_GEOLOCATION_PROJECT/include/ip_geolocation/ip_geolocation_export.h
+++ IP_GEOLOCATION_PROJECT/include/ip_geolocation/ip_geolocation_export.h
@@ -1,1 +1,14 @@
+#ifndef IP_GEOLOCATION_EXPORT_H
+#define IP_GEOLOCATION_EXPORT_H
 
+#ifdef _WIN32
+    #ifdef IP_GEOLOCATION_EXPORTS
+        #define IP_GEOLOCATION_API __declspec(dllexport)
+    #else
+        #define IP_GEOLOCATION_API __declspec(dllimport)
+    #endif
+#else
+    #define IP_GEOLOCATION_API __attribute__((visibility("default")))
+#endif
+
+#endif // IP_GEOLOCATION_EXPORT_H
