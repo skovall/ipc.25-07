41408c7832410fba8e573abc2a174b9894b60624
Nik1tk <89960168401super@gmail.com>
2026-04-06 00:15:52 +0300

    Update НикитаUtils.h

--- src/НикитаUtils.h
+++ src/НикитаUtils.h
@@ -1,1 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <string>
+#include <regex>
+class Utils{
+public:
+  static bool VlIPv4(const std::string& ip);
+  static bool VlIPv6(const std::string& ip);
+  static bool VlIP(const std::string& ip);
+};
 
+#endif 
