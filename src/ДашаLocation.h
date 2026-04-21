64cc3b38adbf8dc81fc82cdc0fa9211dfd4d7df0
Kaimanjj <136440341+Kaimanjj@users.noreply.github.com>
2026-04-21 22:52:11 +0300

    Update ДашаLocation.h

--- src/ДашаLocation.h
+++ src/ДашаLocation.h
@@ -1,12 +1,46 @@
-// === НОВЫЕ ПОЛЯ ДЛЯ ОПРЕДЕЛЕНИЯ VPN ===
-    bool mobile = false;   // мобильный интернет
-    bool proxy = false;    // VPN / Proxy / Tor
-    bool hosting = false;  // IP дата-центра (часто = VPN)
+//проверка на повторный запуск
+#ifndef ДАШАLOCATION_H
+#define ДАШАLOCATION_H
 
-    // Конструктор по умолчанию
-    GeoLocationData() 
-        : ip("N/A"), country("N/A"), countryCode("N/A"), regionName("N/A"),
-          city("N/A"), zip("N/A"), lat(0.0), lon(0.0),
-          timezone("N/A"), isp("N/A"), org("N/A"), as("N/A"),
-          status("N/A"), message("N/A"),
-          mobile(false), proxy(false), hosting(false) {}
+#include <iostream>
+#include <string>
+#include <nlohmann/json.hpp> // подключение библиотеки nlohmann/json.hpp
+#include "TerminalDisplay.h"
+
+using namespace std;
+
+class GeoLocationData {
+public:
+	//объявление переменных
+	string ip;
+	string country;
+	string countryCode;
+	string regionName;
+	string city;
+	string zip;
+	double lat;
+	double lon;
+	string timezone;
+	string isp;
+	string org;
+	string as;
+	string status;
+	string message;
+	bool mobile;
+	bool proxy;
+	bool hosting;
+
+	GeoLocationData() : ip("NONE"), country("NONE"), countryCode("NONE"),
+		regionName("NONE"), city("NONE"), zip("NONE"),
+		lat(0.0), lon(0.0), timezone("NONE"), isp("NONE"),
+		org("NONE"), as("NONE"), status("NONE"), message("NONE") {
+	}
+
+	void display() const;
+};
+
+void to_json(nlohmann::json& j, const GeoLocationData& p);
+void from_json(const nlohmann::json& j, GeoLocationData& p);
+
+
+#endif // !ДАШАLOCATION_H
