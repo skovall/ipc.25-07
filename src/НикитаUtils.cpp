7582f06d2fd6a7e0c5ffbda959ec2181aba2c1d2
nik1tk <89960168401super@gmail.com>
2026-04-21 23:28:53 +0300

    Update НикитаUtils.cpp

--- src/НикитаUtils.cpp
+++ src/НикитаUtils.cpp
@@ -1,27 +1,54 @@
 #include "Utils.h"
-#include <iostream> 
-bool Utils::VlIPv4(const std::string& ip) {
+#include <iostream>
+#include <regex>
+#include <string_view>
+
+namespace {
+    #if __cplusplus >= 202002L
+    constexpr std::regex ipv4_reg(
+        "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}"
+        "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    );
+    
+    constexpr std::regex ipv6_reg(
+        "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|"
+        "([0-9a-fA-F]{1,4}:){1,7}:|"
+        "([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|"
+        "([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|"
+        "([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|"
+        "([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|"
+        "([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|"
+        "[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|"
+        "::([0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){1,6})?)"
+    );
+    #else
     const std::regex ipv4_reg(
         "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}"
         "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
     );
-    return std::regex_match(ip, ipv4_reg);
-}
-bool Utils::VlIPv6(const std::string& ip) {
+    
     const std::regex ipv6_reg(
-        "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|"  
-        "([0-9a-fA-F]{1,4}:){1,7}:|"                   
-        "([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|"  
-        "([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|" 
+        "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|"
+        "([0-9a-fA-F]{1,4}:){1,7}:|"
+        "([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|"
+        "([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|"
         "([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|"
-        "([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|" 
-        "([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|" 
-        "[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|"  
-        "::([0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){1,6})?)" 
+        "([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|"
+        "([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|"
+        "[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|"
+        "::([0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){1,6})?)"
     );
-    return std::regex_match(ip, ipv6_reg);
+    #endif
 }
 
-bool Utils::VlIP(const std::string_view ip) {
+bool Utils::VlIPv4(const std::string_view ip) noexcept {
+    return std::regex_match(ip.begin(), ip.end(), ipv4_reg);
+}
+
+bool Utils::VlIPv6(const std::string_view ip) noexcept {
+    return std::regex_match(ip.begin(), ip.end(), ipv6_reg);
+}
+
+bool Utils::VlIP(const std::string_view ip) noexcept {
     return VlIPv4(ip) || VlIPv6(ip);
 }
