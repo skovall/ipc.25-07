4ae8031080f96604c70823eb3146e04766bf2c05
Kaimanjj <136440341+Kaimanjj@users.noreply.github.com>
2026-04-22 13:31:15 +0300

    Update and rename ДашаLocation.h to Location.h

--- src/Location.h
+++ src/Location.h
@@ -1,31 +1,28 @@
-//проверка на повторный запуск
-#ifndef ДАШАLOCATION_H
-#define ДАШАLOCATION_H
+#ifndef LOCATION_H
+#define LOCATION_H
 
 #include <iostream>
 #include <string>
 #include <nlohmann/json.hpp> // подключение библиотеки nlohmann/json.hpp
 #include "TerminalDisplay.h"
 
-using namespace std;
-
 class GeoLocationData {
 public:
 	//объявление переменных
-	string ip;
-	string country;
-	string countryCode;
-	string regionName;
-	string city;
-	string zip;
+	std::string ip;
+	std::string country;
+	std::string countryCode;
+	std::string regionName;
+	std::string city;
+	std::string zip;
 	double lat;
 	double lon;
-	string timezone;
-	string isp;
-	string org;
-	string as;
-	string status;
-	string message;
+	std::string timezone;
+	std::string isp;
+	std::string org;
+	std::string as;
+	std::string status;
+	std::string message;
 	bool mobile;
 	bool proxy;
 	bool hosting;
@@ -33,7 +30,8 @@ public:
 	GeoLocationData() : ip("NONE"), country("NONE"), countryCode("NONE"),
 		regionName("NONE"), city("NONE"), zip("NONE"),
 		lat(0.0), lon(0.0), timezone("NONE"), isp("NONE"),
-		org("NONE"), as("NONE"), status("NONE"), message("NONE") {
+		org("NONE"), as("NONE"), status("NONE"), message("NONE")
+	{
 	}
 
 	void display() const;
@@ -43,4 +41,4 @@ void to_json(nlohmann::json& j, const GeoLocationData& p);
 void from_json(const nlohmann::json& j, GeoLocationData& p);
 
 
-#endif // !ДАШАLOCATION_H
+#endif // !LOCATION_H
