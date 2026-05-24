f864aa9a84e9aadc9f41d5d5d3227a25f08bd56c
skovall <stetskovall@gmail.com>
2026-05-24 15:14:17 +0300

    Update Utils.h

--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -1,1 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
 
+#include <string_view>
+
+class Utils {
+public:
+    static bool VlIPv4(const std::string_view ip) noexcept;
+    static bool VlIPv6(const std::string_view ip) noexcept;
+    static bool VlIP(const std::string_view ip) noexcept;
+};
+
+#endif // UTILS_H
