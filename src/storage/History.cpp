aab2e47593c7f04ff7f56f8a342f92584b7bf09b
KseniaArefeva <150457796+KseniaArefeva@users.noreply.github.com>
2026-05-25 20:52:04 +0300

    Update History.cpp

--- src/storage/History.cpp
+++ src/storage/History.cpp
@@ -2,3 +2,37 @@
 #include "TerminalDisplay.h"
 #include <fstream>
 #include <iomanip>
+
+using json = nlohmann::json;
+
+History::History(const std::string& file_name)
+    : file_name_(file_name)
+    , max_entries_(100)
+{
+}
+
+bool History::load() {
+    std::ifstream file(file_name_);
+
+    if (!file.is_open()) {
+        return false;
+    }
+
+    try {
+        json j;
+        file >> j;
+
+        entries_.clear();
+
+        if (j.is_array()) {
+            for (const auto& item : j) {
+                entries_.push_back(item.get<GeoLocationData>());
+            }
+        }
+
+        return true;
+    }
+    catch (...) {
+        return false;
+    }
+}
